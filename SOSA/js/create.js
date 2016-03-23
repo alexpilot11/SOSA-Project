7e84b0810c2f709df5696496fbe6045b271ae722
gsuSpidey <davidson201230@gmail.com>
2016-03-23 15:56:15 -0400

    Create create.js
    Javascript for create html page

---
+++ SOSA/js/create.js
@@ -0,0 +1,73 @@
+function showValueR(newValue){
+  document.getElementById("redVal").innerHTML=newValue;
+}
+
+function showValueG(newValue){
+  document.getElementById("greenVal").innerHTML=newValue;
+}
+			
+function showValueB(newValue){
+  document.getElementById("blueVal").innerHTML=newValue;
+}
+
+function colorUpdate() {
+  var r = document.getElementById("redVal").innerHTML;
+  var g = document.getElementById("greenVal").innerHTML;
+  var b = document.getElementById("blueVal").innerHTML;
+  document.getElementById("board").style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+			
+function doAllThisRed(newValue){
+  showValueR(newValue);
+  colorUpdate();
+}
+
+function doAllThisGreen(newValue){
+  showValueG(newValue);
+  colorUpdate();
+}
+			
+function doAllThisBlue(newValue){
+  showValueB(newValue);
+  colorUpdate();
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function myFunction2() {
+	document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
