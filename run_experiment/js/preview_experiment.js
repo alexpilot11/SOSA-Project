9fafcbd2b1426abd844063dfadc15359e0f458de
Alex Channell <achannell@morristechnology.com>
2016-03-26 20:03:36 -0400

    pushing where i'm at. going to dinner. if someone cares, here it is

--- run_experiment/js/preview_experiment.js
+++ run_experiment/js/preview_experiment.js
@@ -132,6 +132,40 @@ function reorderStims() {
 
 }
 
+function showValueR(newValue){
+    document.getElementById("redVal").innerHTML=newValue;
+}
+
+function showValueG(newValue){
+    document.getElementById("greenVal").innerHTML=newValue;
+}
+
+function showValueB(newValue){
+    document.getElementById("blueVal").innerHTML=newValue;
+}
+
+function colorUpdate(boardClass) {
+    var r = document.getElementById("redVal").innerHTML;
+    var g = document.getElementById("greenVal").innerHTML;
+    var b = document.getElementById("blueVal").innerHTML;
+    document.getElementsByClassName(boardClass)[0].style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+function doAllThisRed(newValue, boardClass){
+    showValueR(newValue);
+    colorUpdate(boardClass);
+}
+
+function doAllThisGreen(newValue, boardClass){
+    showValueG(newValue);
+    colorUpdate(boardClass);
+}
+
+function doAllThisBlue(newValue, boardClass){
+    showValueB(newValue);
+    colorUpdate(boardClass);
+}
+
 
 
 
