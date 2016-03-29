9bb1191569e7c687eb3fc3815cf1cc0c03d6b225
ICEEcup <connorwkirkland@yahoo.com>
2016-03-29 19:18:43 -0400

    Tyler's CSS changes

--- SOSA/js/create.js
+++ SOSA/js/create.js
@@ -2,12 +2,24 @@ function showValueR(newValue){
   document.getElementById("redVal").innerHTML=newValue;
 }
 
+function showValueRp(newValue){
+  document.getElementById("redValp").innerHTML=newValue;
+}
+
 function showValueG(newValue){
   document.getElementById("greenVal").innerHTML=newValue;
+}
+
+function showValueGp(newValue){
+  document.getElementById("greenValp").innerHTML=newValue;
 }
 			
 function showValueB(newValue){
   document.getElementById("blueVal").innerHTML=newValue;
+}
+
+function showValueBp(newValue){
+  document.getElementById("blueValp").innerHTML=newValue;
 }
 
 function colorUpdate() {
@@ -16,20 +28,42 @@ function colorUpdate() {
   var b = document.getElementById("blueVal").innerHTML;
   document.getElementById("board").style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 }
+
+function colorUpdatep() {
+  var r = document.getElementById("redValp").innerHTML;
+  var g = document.getElementById("greenValp").innerHTML;
+  var b = document.getElementById("blueValp").innerHTML;
+  document.getElementById("boardp").style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+}
 			
 function doAllThisRed(newValue){
   showValueR(newValue);
   colorUpdate();
 }
 
+function doAllThisRedp(newValue){
+  showValueRp(newValue);
+  colorUpdatep();
+}
+
 function doAllThisGreen(newValue){
   showValueG(newValue);
   colorUpdate();
+}
+
+function doAllThisGreenp(newValue){
+  showValueGp(newValue);
+  colorUpdatep();
 }
 			
 function doAllThisBlue(newValue){
   showValueB(newValue);
   colorUpdate();
+}
+
+function doAllThisBluep(newValue){
+  showValueBp(newValue);
+  colorUpdatep();
 }
 
 /* When the user clicks on the button, 
@@ -59,7 +93,7 @@ window.onclick = function(event) {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.dropbtn2')) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content2");
     var i;
