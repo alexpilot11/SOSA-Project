3c5419235b4b12985cd2f55aa1078caa5350ddf9
ICEEcup <connorwkirkland@yahoo.com>
2016-04-23 21:09:48 -0400

    Unit Testing
    Added a couple files Tyler and I are working on for the unit tests

---
+++ js/test.js
@@ -0,0 +1,119 @@
+QUnit.test( "init", function( assert ) {
+	assert.equal( info.style.position, 'something else', "container is equal to document.createElement('div')" );
+});
+
+QUnit.test( "onWindowResize", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "onDocumentMouseMove", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "onDocumentMouseDown", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "onDocumentMouseUp", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "animate", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "render", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "getTodaysDate", function( assert ) {
+	assert.deepEqual( getTodaysDate(), "04/23/2016", "Passed" );
+});
+
+QUnit.test( "updateLabels", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "createStimBoard", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "createViewStimuli", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "viewStimuli", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "createBeginExperiment", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "centerStimsOnBoard", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "createFinishExperiment", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "finishExperiment", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "createGUI", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "readExperimentFromFile", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "convertExperimentToThing", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "makeStims", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "makeLabel", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "overwriteLabel", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "updateBoard", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "wrapText", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "setBoardColor", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "setBackgroundColor", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "setLabelColor", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "setLabelSize", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "toggleLabels", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
+
+QUnit.test( "calculateDistance", function( assert ) {
+	assert.ok( true, "Not Done" );
+});
